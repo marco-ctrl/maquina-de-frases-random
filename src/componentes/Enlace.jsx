d6e4ad8eabe2fba4cc75a1c636a5bbf455d121fd
marco <mamanivillenam.com>
2022-06-22 21:47:25 -0400

    primer commit

--- src/componentes/Enlace.jsx
+++ src/componentes/Enlace.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { AiFillTwitterSquare } from "react-icons/ai";
-import { FaTumblrSquare } from "react-icons/fa";
 
 function Enlace(props){
     return (
@@ -12,14 +10,6 @@ function Enlace(props){
           >
           {props.children}
         </a>
-      /*<a className="button" 
-          id="tweet-quote" 
-          title="Tweet this quote!" 
-          target="_top" 
-          href="https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22Life%20shrinks%20or%20expands%20in%20proportion%20to%20one%E2%80%99s%20courage.%22%20Anais%20Nin"
-          >
-          <AiFillTwitterSquare className="fa" />
-        </a>*/
     );
   }
 
